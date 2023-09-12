63968bd7b8c4aa8dee5237622ebe135a64aa276d
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2023-09-12 23:16:12 +0100

    Working permissions for basic ping command

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ require("dotenv").config(); // Load environment variables from .env file
 const { Client, IntentsBitField } = require("discord.js"); // Import required classes from discord.js
 
 const client = new Client({
-  intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
-client.once("ready", () => {
-  console.log("Bot is online!");
+client.once("ready", (c) => {
+  console.log(`Bot is online and logged in as ${c.user.tag}!`);
 });
 
-client.on("message", (message) => {
+client.on("messageCreate", (message) => {
   if (message.content === "!ping") {
     message.channel.send("Pong!");
   }
