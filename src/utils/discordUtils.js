68a092d0948d3add67855c1f82dfd3169c5e92dc
kmanjt <kevin.tomescu2@mail.dcu.ie>
2023-10-23 11:44:17 +0100

    Fix broken daily and weekly timetable hours

--- src/utils/discordUtils.js
+++ src/utils/discordUtils.js
@@ -6,7 +6,10 @@ const {
 const { getAllTasksForUser } = require("../repositories/sqlRepository");
 
 function createDailyTimetableEmbed() {
-  const todayClasses = getTodayClasses();
+  const todayClassesObj = getTodayClasses();
+  const sortedTimes = Object.keys(todayClassesObj).sort((a, b) =>
+    a.localeCompare(b)
+  );
   const dateObj = new Date();
   // set day in English
   const day = new Intl.DateTimeFormat("en-EU", { weekday: "long" }).format(
@@ -19,35 +22,36 @@ function createDailyTimetableEmbed() {
     .setTimestamp()
     .setFooter({ text: "Timetable" });
 
-  for (const [time, classInfo] of Object.entries(todayClasses)) {
+  sortedTimes.forEach((time) => {
+    const classInfo = todayClassesObj[time];
     const fieldName = `${time} - ${classInfo.module}`;
     const fieldValue = `Duration: ${classInfo.duration} mins\nType: ${classInfo.type}\nBuilding: ${classInfo.building}\nRoom: ${classInfo.room}`;
     embed.addFields({ name: fieldName, value: fieldValue });
-  }
+  });
+
   return embed;
 }
 
 function createWeeklyTimetableEmbed() {
-  const weekClasses = getWeekClasses();
+  const weekClassesObj = getWeekClasses();
   const embed = new EmbedBuilder()
     .setColor(0x0099ff)
     .setTitle("This Week's COMSCI3 Classes")
     .setTimestamp()
     .setFooter({ text: "Timetable" });
 
-  // Loop through each day
-  for (const [day, timeSlots] of Object.entries(weekClasses)) {
-    // Add a header for each day
+  Object.entries(weekClassesObj).forEach(([day, classes]) => {
     embed.addFields({ name: day, value: "\u200B" });
-
-    // Loop through each time slot for that day
-    for (const [time, classInfo] of Object.entries(timeSlots)) {
+    const sortedTimes = Object.keys(classes).sort((a, b) => a.localeCompare(b));
+    sortedTimes.forEach((time) => {
+      const classInfo = classes[time];
       const fieldName = `${time} - ${classInfo.module}`;
       const fieldValue = `Duration: ${classInfo.duration} mins\nType: ${classInfo.type}\nBuilding: ${classInfo.building}\nRoom: ${classInfo.room}`;
       embed.addFields({ name: fieldName, value: fieldValue });
-    }
+    });
     embed.addFields({ name: "\u200B", value: "\n\u200B" });
-  }
+  });
+
   return embed;
 }
 
